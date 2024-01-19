f702aa1eee16540ac4c6d3dfd977daedeeafbae8
Oleksandr Martynov <martynov.aleksandr@gmail.com>
2024-01-19 14:10:10 -0800

    arrow commit

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -15,8 +15,23 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-// Your code here 
+// Your code here
+// function addFive(number) {
+//   return number + 5
+// }
+// let addFive2 = function addFive2(number) {
+//   return number + 5;
+// }
+// let addFive3 = function(number) {
+//   return number + 5}
 
+// let addFive4 = (number) => {return number + 5}
+
+let addFive = number => number + 5 ;
+
+let result1 = addFive(0); // returns 5
+let result2 = addFive(10); // returns 15
+let result3 = addFive(37); // returns 42
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addFive;
