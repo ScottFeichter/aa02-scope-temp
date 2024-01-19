99b87410f5925b4bab056586f72f8fdbba35e529
Scott Feichter <scottfeichter@gmail.com>
2024-01-19 14:27:53 -0800

    02 finish

--- problems/02-arrow-my-map.js
+++ problems/02-arrow-my-map.js
@@ -18,7 +18,13 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 ***********************************************************************/
 
-// Your code here 
+const arrowMyMap = (array, callback) => {
+  let result = [];
+  array.forEach((element) => {
+    result.push(callback(element));
+  });
+  return result;
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
